8b6ed0f89635c863ed63281ed73bdbe19e5c5cfc
Bryan Hughes <bryan@nebr.us>
2025-11-05 14:54:14 -0800

    Set up scaffolding

---
+++ eslint.config.mjs
@@ -0,0 +1,46 @@
+import { getDirname } from 'cross-dirname';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import { all } from 'eslint-plugin-fast-import';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  eslintPluginPrettierRecommended,
+
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'object-shorthand': 'error',
+
+      // Handled by TypeScript eslint
+      'no-unused-vars': 'off',
+    },
+  },
+
+  all({
+    entryPoints: {
+      'src/app/**/page.tsx': ['default'],
+      'src/app/**/layout.tsx': ['default', 'metadata'],
+      '*config*': ['default'],
+    },
+    rootDir: getDirname(),
+  }),
+]);
+
+export default eslintConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="absolute inset-0 p-3 dark:bg-black">
+      <h1>Home</h1>
+    </div>
+  );
+}
